de1cdf27c0371f176b3ba7fb02fe0e92631b2640
staslif <149203375+staslif@users.noreply.github.com>
2024-01-11 23:03:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program1
+{
+    static void Main(string[] args){
+        // Запросить у пользователя путь к файлу для чтения
+        Console.Write("Введите путь к текстовому файлу: ");
+        string filePath = Console.ReadLine();
+
+        // Проверка, существует ли указанный файл
+        if (File.Exists(filePath)){
+            try{
+                // Прочитать содержимое файла
+                string fileContent = File.ReadAllText(filePath);
+
+                // Записать содержимое в новый файл в папке с проектом
+                string newFilePath = "new_file.txt";
+                File.WriteAllText(newFilePath, fileContent);
+
+                Console.WriteLine("Файл успешно записан.");
+            }
+            catch (Exception ex){
+                Console.WriteLine("Ошибка при чтении или записи файла: " + ex.Message);
+            }
+        }
+        else{
+            Console.WriteLine("Указанный файл не существует.");
+        }}}
