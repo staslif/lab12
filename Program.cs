8a38bc12af1b51810ead459b4bca46930cb51ae0
staslif <149203375+staslif@users.noreply.github.com>
2024-05-17 19:51:15 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,13 @@
-﻿class Program1
+Console.WriteLine("укажите путь"); //E:\\DOCUMENT\\12.txt
+string path = Console.ReadLine();
+string path1 = Directory.GetCurrentDirectory();
+if (File.Exists(path1))
 {
-    static void Main(string[] args){
-        // Запросить у пользователя путь к файлу для чтения
-        Console.Write("Введите путь к текстовому файлу: ");
-        string filePath = Console.ReadLine();
-
-        // Проверка, существует ли указанный файл
-        if (File.Exists(filePath)){
-            try{
-                // Прочитать содержимое файла
-                string fileContent = File.ReadAllText(filePath);
-
-                // Записать содержимое в новый файл в папке с проектом
-                string newFilePath = "new_file.txt";
-                File.WriteAllText(newFilePath, fileContent);
-
-                Console.WriteLine("Файл успешно записан.");
-            }
-            catch (Exception ex){
-                Console.WriteLine("Ошибка при чтении или записи файла: " + ex.Message);
-            }
-        }
-        else{
-            Console.WriteLine("Указанный файл не существует.");
-        }}}
+string a = File.ReadAllText(path);
+File.WriteAllText(path1 + "\\12.txt", a);
+Console.WriteLine(path1 + "\\12.txt");
+}
+else
+{
+Console.WriteLine("ошибка");
+}
